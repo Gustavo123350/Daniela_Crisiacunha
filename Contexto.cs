77225818276dd2a548e07f40d05ea932fa69bf93
Gustavo123350 <73312915+Gustavo123350@users.noreply.github.com>
2022-11-05 01:48:35 -0300

    cummit main

---
+++ Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Salão.Models;
+
+namespace Salão
+{
+    public class Contexto : DbContext
+    {
+        
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<administracao> administracao { get; set; }
+        public DbSet<cadastro_cliente> cadastro_cliente { get; set; }
+        public DbSet<cliente> cliente { get; set; }
+        public DbSet<estoque> estoque { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Salão
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<Contexto>(options => options.UseSqlServer("Data Source=DESKTOP-4TE3GG6;Initial Catalog=salao;Integrated Security=True"));
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
